9984de62adc4cd493441ab5d716730ee02eb2814
qwavy <nursultanr585@gmail.com>
2024-10-14 23:29:45 +0500

    init project

---
+++ cinema-booking-react/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from "@/components/ui/button.tsx";
+
+const App = () => {
+    return (
+        <div>
+            <h1>
+            </h1>
+        </div>
+    );
+};
+
+export default App;
